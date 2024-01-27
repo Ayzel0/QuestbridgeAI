defa8ad97d0d25824db4cf0b6f6d7b9abad39305
ayzel <andrewzlee@gmail.com>
2024-01-27 13:55:06 -0500

    updates

---
+++ scripts/testing_openai.py
@@ -0,0 +1,13 @@
+import tiktoken
+
+# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
+tokenizer = tiktoken.get_encoding("cl100k_base")
+
+df = pd.read_csv('processed/scraped.csv', index_col=0)
+df.columns = ['title', 'text']
+
+# Tokenize the text and save the number of tokens to a new column
+df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+
+# Visualize the distribution of the number of tokens per row using a histogram
+df.n_tokens.hist()
