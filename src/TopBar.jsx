4cdb165b7a600edff2d97126e75fd4e8edafe2c2
ayzel <andrewzlee@gmail.com>
2024-01-27 23:23:18 -0500

    fix logo size

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -2,11 +2,11 @@ import MainLogo from './assets/mainLogo.png';
 
 const TopBar = () => {
   return (
-    <div>
-      <div className='absolute top-2 left-10 w-20'>
+    <div className='h-32 bg-cyan-800 flex flex-col justify-center'>
+      <div className='absolute top-2 left-10 w-[110px]'>
         <a href='/'><img src={MainLogo} /></a>
       </div>
-      <div className='flex flex-row-reverse bg-cyan-800'>
+      <div className='flex flex-row-reverse bg-inherit'>
         <div className='flex flex-row-reverse mr-12'>
           <a 
             className='underline-offset-4 hover:underline-offset-8 underline transition-all text-2xl my-8 mx-6 cursor-pointer text-white'
