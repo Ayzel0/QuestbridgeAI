0f9d42c1a5b60c4e786cd90954a420ef71411f70
ayzel <andrewzlee@gmail.com>
2024-01-27 22:45:59 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 const viteEnv = {};
 Object.keys(process.env).forEach((key) => {
   if (key.startsWith('VITE_')) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key];
+    viteEnv[`import.meta.env.${key}`] = JSON.stringify(process.env[key]);
   }
 });
 export default {
